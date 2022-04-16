c9e6cd7a8d43476102c0108b4a141dd0fff78119
d3rroh <micaslounge@gmail.com>
2022-04-16 16:06:24 +0300

    gh

--- main.h
+++ main.h
@@ -1,42 +1,43 @@
-#ifndef _PRINT_FUNCTION_H_
-#define _PRINT_FUNCTION_H_
-#include <stdarg.h>
+#ifndef MAIN_H
+#define MAIN_H
 
-int _printf(const char *format, ...);
-int pull_print(char c, va_list *ap);
-
-int _putchar(char);
-int buffer(char a, char *s);
-int p_bin(unsigned int num);
-int exponent(int x, int y);
-int p_int(int);
-int p_string(char *);
-int p_hexstring(char *);
-int p_rev(char *s);
-int p_hexcap(unsigned int num);
-int p_octal(unsigned int num);
-int p_uint(unsigned int num);
-int p_hex(unsigned int num);
-
-int makestring(va_list *);
-int makechar(va_list *);
-int makeint(va_list *);
-int makebin(va_list *);
-int makehex(va_list *);
-int makehexcap(va_list *);
-int makeoctal(va_list *);
-int makehexstring(va_list *);
-int makeunsigned(va_list *);
-
+#include <stdarg.h> /* va_list, va_start, va_end, va_arg, va_copy*/
+#include <stddef.h> /*typedef*/
 /**
- * struct flag -flag object
- * @letter: flag char
- * @prnt: print function pointer
- * Descriptions: contains flag letter and corresponding print function
- **/
-typedef struct flag
+ * struct printer - Define structure of data
+ * according to give a printf.
+ *
+ * @format:format of data.
+ * @func: pointer to a function.
+*/
+
+typedef struct printer
 {
-	char letter;
-	int (*prnt)(va_list *);
-} find_flag;
-#endif
+	char format;
+	int (*func)(va_list *);
+} printer_t;
+
+/*Old functions that can help us*/
+int _putchar(char c);
+
+/*Function for select a function acording a format*/
+printer_t choose_func(char format);
+
+/*Principal Prootype*/
+int _printf(const char *format, ...);
+
+/*New prototypes for printf acording a format*/
+int _printf_char(va_list *);
+int _printf_string(va_list *);
+int _printf_int(va_list *);
+int _printf_unsigned_int(va_list *);
+int _printf_double(va_list *);
+int _printf_octal(va_list *);
+int _printf_hexa(va_list *);
+int _printf_mini_hexa(va_list *);
+int _printf_binary(va_list *);
+int _printf_special(va_list *);
+int _printf_rot13(va_list *params);
+int _printf_reverse(va_list *);
+
+#endif /*MAIN_H*/
