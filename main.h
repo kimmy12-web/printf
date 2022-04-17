efd00f62d723c6dab49eda2b3711e03f57e3abf4
d3rroh <micaslounge@gmail.com>
2022-04-17 15:31:54 +0300

    gh

--- main.h
+++ main.h
@@ -17,7 +17,6 @@ typedef struct fmt
 } fmt_t;
 
 int _printf(const char *format, ...);
-<<<<<<< HEAD
 int print_op(const char *format, fmt_t *print_arr, va_list list);
 int ch(va_list character);
 int str(va_list string);
@@ -32,22 +31,4 @@ int _strlen(char *s);
 int _bin(va_list bin);
 int _putchar(char c);
 #endif
-=======
 
-/*New prototypes for printf acording a format*/
-int _printf_char(va_list *);
-int charsFormats(const char *format, va_list args);
-int _printf_string(va_list *);
-int _printf_int(va_list *);
-int _printf_unsigned_int(va_list *);
-int _printf_double(va_list *);
-int _printf_octal(va_list *);
-int _printf_hexa(va_list *);
-int _printf_mini_hexa(va_list *);
-int _printf_binary(va_list *);
-int _printf_special(va_list *);
-int _rot13(va_list rot);
-int _printf_reverse(va_list *);
-
-#endif /*MAIN_H*/
->>>>>>> c4669034be233364c920b5515b79f9d7bd699c63
