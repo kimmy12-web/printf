fb0281edfdd1a2032a7b9217b296fd093d7866fd
kimmy12-web <kimmymubiru12@gmail.com>
2022-04-18 18:28:19 +0300

    _printf

--- _printf.c
+++ _printf.c
@@ -2,7 +2,7 @@
 
 /**
  * _printf - produces output according to a format
- * @format: format string containing the characters and the specifiers
+ * @format: formats string containing the characters and the specifiers
  * Description: this function will call the get_print() function that will
  * determine which printing function to call depending on the conversion
  * specifiers contained into fmt
